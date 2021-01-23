3eeb986ddec07c62ea3a2640a3402768f2d5fcf5
Alejandro Aranda <alexwing@gmail.com>
2021-01-23 12:23:38 +0100

    first commit

---
+++ Assets/Models/Earth/earthToHead.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earthToHead : MonoBehaviour {
+    public Transform Spawn;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Spawn.transform.position;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# MoonWalk-vrtk-oculus-go
+
+UNITY3D OCULUS GO/GEARVR APP of Moon Terrain using VRTK 
+
+# Requirements
+
+* Oculus Go
+* Unity 
+
+Open it With Unity go to Scenes\MoonWalk
+
+Teleportation is like steamvr using touchpad.
+There are in interactable object and gun, try it all.
+Grab button is  touchpad_press.
+
